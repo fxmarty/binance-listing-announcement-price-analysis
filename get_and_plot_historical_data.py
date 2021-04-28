be9d08b43e345816c38bb8f1c366dc789fe091b6
FxMarty <9808326+fxmarty@users.noreply.github.com>
2021-04-28 22:30:43 +0200

    added plots

--- get_and_plot_historical_data.py
+++ get_and_plot_historical_data.py
@@ -1,10 +1,22 @@
 import ccxt
 
+import mplfinance as mpf
+import numpy as np
 import pandas as pd
 
 import datetime
 import pytz
 
+import os
+
+from candles import convert_ohlcv_from_1m_to, plot_candles
+
+'''
+Check a symbol `substring` (e.g. 'ABC') in a list of pairs, for example
+str_list = ['ABC/USD', 'BTC/USD', 'USD/EUR', 'EUR/ABC']
+
+In this example returns ['ABC/USD', 'EUR/ABC']
+'''
 def check_substring_presence(substring, str_list):
     res = []
     for i, string in enumerate(str_list):
@@ -50,7 +62,15 @@ for index, row in df_listing.iterrows():
     local_time_formatted = fr_timezone.localize(local_time_formatted)
     utc_time = local_time_formatted.astimezone(utc_timezone)
 
+    dir_path = os.path.join('dat/listings_data',
+                            utc_time.strftime('%Y-%m-%dT%H:%MZ') + '_' + symbol)
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
     print('Listing time UTC:', utc_time)
+
+    # we look ahead 20 minutes for context
+    utc_time_shifted = utc_time - datetime.timedelta(hours=0, minutes=20)
 
     present_in_exchanges = dict()
     for exchange_name in markets_dict:
@@ -60,31 +80,50 @@ for index, row in df_listing.iterrows():
 
         valid_represented_pairs = []
         for pair in represented_pairs:
+            pair_formatted = pair.replace('/', '-')
 
             try:
                 candles = exchange.fetch_ohlcv(
                         pair,
                         timeframe='1m',
-                        since=exchange.parse8601(utc_time.isoformat()))
+                        since=exchange.parse8601(utc_time_shifted.isoformat()))
             except Exception as e:
                 print('Could not get candle:', e)
                 continue
 
             try:
-                #print(candles[0])
-                #print(exchange.iso8601(candles[0][0]))
-
                 # check the returned time from the exchange corresponds to the
                 # listing time on Binance
-                cond = exchange.iso8601(candles[0][0])[:13] == utc_time.isoformat()[:13]
+                cond = exchange.iso8601(candles[0][0])[:13] == utc_time_shifted.isoformat()[:13]
 
-                # we have sensible data available
+                # we have sensible data available, i.e. volume is not zero and
+                # the price is not 0. For now, only save figures but data analysis
+                # could come later
                 cond = cond and candles[0][1] != 0 and candles[0][-1] != 0
                 if cond:
                     print([exchange.iso8601(candles[0][0])] + candles[0][1:])
+
+                    file_name = exchange_name + '_' + pair_formatted + '_1m'
+                    file_path = os.path.join(dir_path, file_name + '.png')
+                    plot_candles(candles[:150], title=file_name,
+                                 index_marker=20, save_path=file_path)
+
+                    res = convert_ohlcv_from_1m_to('5m', candles)
+                    file_name = exchange_name + '_' + pair_formatted + '_5m'
+                    file_path = os.path.join(dir_path, file_name + '.png')
+                    plot_candles(res[:150], title=file_name,
+                                 index_marker=4, save_path=file_path)
+
+                    res = convert_ohlcv_from_1m_to('15m', candles)
+                    file_name = exchange_name + '_' + pair_formatted + '_15m'
+                    file_path = os.path.join(dir_path, file_name + '.png')
+                    plot_candles(res, title=file_name, save_path=file_path)
                 else:
+                    '''
+                    print('NOT GOOD:',
+                           [exchange.iso8601(candles[0][0])] + candles[0][1:])
+                    '''
                     continue
-                    #print('NOT GOOD:', [exchange.iso8601(candles[0][0])] + candles[0][1:])
 
             except Exception as e:
                 print('Could not print candle (' + exchange_name + '):', e)
--- price_history/ccxt_test1.py
+++ price_history/ccxt_test1.py
@@ -6,6 +6,35 @@ candles = exchange.fetch_ohlcv('BTC/USDT',
 #print(exchange.has['fetchOHLCV'])
 print(candles[0])
 print(exchange.iso8601(candles[0][0]))
+
+##
+import mplfinance as mpf
+import numpy as np
+import pandas as pd
+
+##
+candles_array = np.array(candles)
+
+index_df = pd.DataFrame({'Date': candles_array[:, 0].astype(int)})
+index_df['Date'] = pd.to_datetime(index_df['Date'], unit='ms')
+
+
+##
+dataset = pd.DataFrame({'Open': candles_array[:, 1],
+                        'High': candles_array[:, 2],
+                        'Low': candles_array[:, 3],
+                        'Close': candles_array[:, 4],
+                        'Volume': candles_array[:, 5]},
+                       index=pd.DatetimeIndex(index_df['Date']))
+
+dataset.index.name = 'Date'
+
+##
+
+save_params = dict(fname='/home/felix/saved_file_candles.jpg',
+                   dpi=300, bbox_inches='tight')
+mpf.plot(dataset[:500], type='candle', volume=True, show_nontrading=True,
+         style='charles', savefig=save_params)
 ##
 #print(ccxt.exchanges)
 
@@ -35,3 +64,76 @@ print(candles[0])
 print(exchange.iso8601(candles[0][0]))
 
 ##
+
+
+def convert_ohlcv_from_1m_to(precision, data_list):
+    n_aggregate = -1
+    if precision == '5m':
+        n_aggregate = 5
+    elif precision == '15m':
+        n_aggregate = 15
+    elif precision == '30m':
+        n_aggregate = 30
+    else:
+        raise ValueError('Conversion not supported.')
+
+    res = []
+    current_min = np.inf
+    current_max = - np.inf
+    current_open = -1
+    current_close = -1
+    current_time = -1
+    current_volume = 0
+
+    length_result = len(data_list) // n_aggregate
+
+    for i in range(length_result):
+        current_time = data_list[i * n_aggregate][0]
+        current_open = data_list[i * n_aggregate][1]
+        for j in range(n_aggregate):
+            current_min = min(current_min,
+                              data_list[i * n_aggregate + j][3])
+            current_max = max(current_max,
+                              data_list[i * n_aggregate + j][4])
+            current_volume += data_list[i * n_aggregate + j][5]
+        current_close = data_list[i * n_aggregate + (n_aggregate - 1)][2]
+
+        res.append([current_time, current_open, current_max,
+                    current_min, current_close, current_volume])
+
+        current_min = np.inf
+        current_max = - np.inf
+        current_open = -1
+        current_close = -1
+        current_time = -1
+        current_volume = 0
+
+    return res
+##
+def plot_candles(candles_list):
+    candles_array = np.array(candles_list)
+
+    index_df = pd.DataFrame({'Date': candles_array[:, 0].astype(int)})
+    index_df['Date'] = pd.to_datetime(index_df['Date'], unit='ms')
+
+    dataset = pd.DataFrame({'Open': candles_array[:, 1],
+                            'High': candles_array[:, 2],
+                            'Low': candles_array[:, 3],
+                            'Close': candles_array[:, 4],
+                            'Volume': candles_array[:, 5]},
+                        index=pd.DatetimeIndex(index_df['Date']))
+
+    dataset.index.name = 'Date'
+
+    save_params = dict(fname='/home/felix/saved_file_candles.jpg',
+                    dpi=300, bbox_inches='tight')
+    mpf.plot(dataset, type='candle', volume=True, show_nontrading=True,
+            style='charles') #, savefig=save_params)
+
+##
+res = convert_ohlcv_from_1m_to('5m', candles_array)
+
+##
+plot_candles(candles_array)
+##
+plot_candles(res)
